74f936f98c3cfb4cfcc8cc319206933ca92c5c4d
Sameep Sehgal <98826161+sehgal-sameep@users.noreply.github.com>
2025-08-14 11:07:07 +0530

    initial commit
    basic project structure

---
+++ aws-cognito-authentication/src/main/java/com/cognito/AwsCognitoAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.cognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCognitoAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCognitoAuthenticationApplication.class, args);
+	}
+
+}
---
+++ aws-cognito-authentication/src/test/java/com/cognito/AwsCognitoAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCognitoAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
